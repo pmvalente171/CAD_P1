45f51aee7b1f2ec6cce9414854a1f0124ca8822d
Pedro Valente <pm.valente@campus.fct.unl.pt>
2020-11-20 20:57:13 +0000

    started implementing streams, they don't work

--- src/lab3/cuda_nbody_all_pairs.cu
+++ src/lab3/cuda_nbody_all_pairs.cu
@@ -10,6 +10,7 @@ static constexpr int BLOCK_WIDTH  = 256;
 static constexpr int BLOCK_HEIGHT = 1;
 
 static constexpr int thread_block_size = 512;
+static constexpr int n_stream_width = 5, n_stream_height = 5;
 
 namespace cadlabs {
 
@@ -64,6 +65,7 @@ namespace cadlabs {
 
             particle_t *fp_1 = &particles[forceParticle], *fp_2 = &particles[forceParticle + blockDim.x];
             particle_t *tp = &particles[targetParticle];
+            printf("values : %.6f ; %.6f\n", tp->x_pos, tp->y_pos);
 
             double x_sep_1 = fp_1->x_pos - tp->x_pos, x_sep_2 = fp_2->x_pos - tp->x_pos;
             double y_sep_1 = fp_1->y_pos - tp->y_pos, y_sep_2 = fp_2->y_pos - tp->y_pos;
@@ -153,34 +155,62 @@ namespace cadlabs {
 
     void cuda_nbody_all_pairs::calculate_forces() {
         uint size = number_particles * sizeof(particle_t);
+        cudaStream_t streams [n_stream_height * n_stream_width];
 
-        cudaMemcpy(gpu_particles, particles, size, cudaMemcpyHostToDevice);
+        for (auto & stream : streams)
+            cudaStreamCreate(&stream);
 
-        dim3 grid(gridWidth, gridHeight);
-        // printf("grid dims: %d, %d\n", gridWidth, gridHeight);
-        dim3 block(BLOCK_WIDTH, BLOCK_HEIGHT);
-        ::cadlabs::calculate_forces<<<grid, block>>>(gpu_particles, dForcesX, dForcesY, number_particles, gridWidth);
-        //printf("\n\n");
+        int stream_size_x = number_particles / n_stream_width +
+                (number_particles % n_stream_width != 0);
+        int stream_size_y = number_particles / n_stream_height +
+                (number_particles % n_stream_height!= 0);
 
-        cudaMemcpy(hForcesX, dForcesX, number_particles * gridWidth * sizeof(double), cudaMemcpyDeviceToHost);
-        cudaMemcpy(hForcesY, dForcesY, number_particles * gridWidth * sizeof(double), cudaMemcpyDeviceToHost);
+        for (int i=0; i<n_stream_height; i++) {
+            int pos_i = i * n_stream_width;
+            for (int j = 0; j < n_stream_width; j++) {
+                int index = pos_i + j;
+                int stream_offset_x = j * stream_size_x, stream_offset_y = i * stream_size_y;
+                dim3 temp_grid(stream_size_x / gridWidth + (stream_size_x % gridWidth != 0),
+                               stream_size_y / gridHeight + (stream_size_y % gridHeight != 0));
+                dim3 block(BLOCK_WIDTH, BLOCK_HEIGHT);
+                // TODO Isto provavelmente está mal
+                cudaMemcpyAsync(&gpu_particles[stream_offset_x],
+                                &particles[stream_offset_x],
+                                stream_size_x * stream_size_y * sizeof(particle_t),
+                                cudaMemcpyHostToDevice, streams[index]);
 
-        /*for (int i = 0; i < number_particles; i++) {
-            int targetParticle = i * gridWidth;
-            double xF = 0; double yF = 0;
-            for (int j = 0; j < gridWidth; j++) {
-                printf("Particle forces : %.3f ; %.3f\n", hForcesX[targetParticle + j],
-                       hForcesY[targetParticle + j]);
+                ::cadlabs::calculate_forces<<<temp_grid, block, 0, streams[index]>>>(
+                        &gpu_particles[stream_offset_x ],
+                        &dForcesX[stream_offset_x + stream_offset_y * gridWidth],
+                        &dForcesY[stream_offset_x + stream_offset_y * gridWidth],
+                        stream_size_x * stream_size_y,
+                        stream_size_x / gridWidth + (stream_size_x % gridWidth != 0));
+
+                cudaMemcpyAsync(&hForcesX[stream_offset_x + stream_offset_y * gridWidth],
+                                &dForcesX[stream_offset_x + stream_offset_y * gridWidth],
+                                stream_size_x * stream_size_y * gridWidth * sizeof(double),
+                                cudaMemcpyDeviceToHost, streams[index]);
+                cudaMemcpyAsync(&hForcesY[stream_offset_x + stream_offset_y * gridWidth],
+                                &dForcesY[stream_offset_x + stream_offset_y * gridWidth],
+                                stream_size_x * stream_size_y * gridWidth * sizeof(double),
+                                cudaMemcpyDeviceToHost, streams[index]);
             }
-        }*/
+        }
+
+        for (auto & stream : streams)
+            cudaStreamSynchronize(stream);
+
+        // cudaMemcpy(gpu_particles, particles, size, cudaMemcpyHostToDevice);
+        // dim3 grid(gridWidth, gridHeight);
+        // dim3 block(BLOCK_WIDTH, BLOCK_HEIGHT);
+        // ::cadlabs::calculate_forces<<<grid, block>>>(gpu_particles, dForcesX, dForcesY, number_particles, gridWidth);
+        // cudaMemcpy(hForcesX, dForcesX, number_particles * gridWidth * sizeof(double), cudaMemcpyDeviceToHost);
+        // cudaMemcpy(hForcesY, dForcesY, number_particles * gridWidth * sizeof(double), cudaMemcpyDeviceToHost);
 
         for (int i = 0; i < number_particles; i++) {
             int targetParticle = i * gridWidth;
             double xF = 0; double yF = 0;
             for (int j = 0; j < gridWidth; j++) {
-                // printf("index %d (i:%d, j:%d)\n", targetParticle + j, i, j);
-                // printf("hForcesX[%d] = %f, for particle %d\n", i, hForcesX[targetParticle + j], targetParticle);
-                // printf("hForcesX[%d] = %f, for particle %d\n", i, hForcesX[targetParticle + j], targetParticle);
                 xF += hForcesX[targetParticle + j];
                 yF += hForcesY[targetParticle + j];
             }
